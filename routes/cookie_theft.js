477bed153348685b2d01136a0cf9e2069a7260cc
Kaicheng Tan <tan95@cs.washington.edu>
2017-05-09 15:26:55 -0700

    delete node modules

--- routes/cookie_theft.js
+++ routes/cookie_theft.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('Hello World :)', { title: 'CSE484 Lab2' });
+
+  res.send('Your cookie is parsed :)');
 });
 
 module.exports = router;
