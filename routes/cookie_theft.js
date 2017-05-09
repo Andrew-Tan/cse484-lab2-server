025f0b1992be979cf5191fb7920c23bdad5cf8cb
Kaicheng Tan <tan95@cs.washington.edu>
2017-05-09 15:32:57 -0700

    cookie theft :P

--- routes/cookie_theft.js
+++ routes/cookie_theft.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-
+  console.log('\n\nCookie received by cookie theft:');
+  console.log(JSON.stringify(req.cookies, null, 2), '\n\n');
   res.send('Your cookie is parsed :)');
 });
 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Hello World :)', { title: 'CSE484 Lab2' });
+  res.render('index', { title: 'CSE484 Lab2' });
 });
 
 module.exports = router;
